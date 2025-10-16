a4b826e9ec629f92c165c440f42aba315f67ff25
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-16 17:56:31 -0500

    Add knightsTravails with initial func declarations

---
+++ knightsTravails.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+function getValidMoves(x, y) {
+  // logic
+}
+
+function isValidPosition(x, y) {
+  return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+}
+
+function knightMoves(start, end) {
+  // main logic
+}
+
+export { knightMoves };

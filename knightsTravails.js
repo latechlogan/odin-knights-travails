83cf2a24c3af93539743ef26132dbe5a44793d78
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-16 23:51:35 -0500

    Add logic and return formatting to knightMoves

--- knightsTravails.js
+++ knightsTravails.js
@@ -21,12 +21,40 @@ function isValidPosition(position) {
   return x >= 0 && x <= 7 && y >= 0 && y <= 7;
 }
 
-function knightMoves(start, end) {
+function knightMoves(start, target) {
   let queue = [{ position: start, path: [start] }];
   let visited = new Set();
   visited.add(start.join());
 
-  return visited;
+  while (queue.length) {
+    let currentSpace = queue.shift();
+
+    if (currentSpace.position.join() === target.join()) {
+      const moves = currentSpace.path.length - 1;
+      console.log(
+        `You made it in ${moves} move${
+          moves === 1 ? "" : "s"
+        }!  Here's your path:`
+      );
+      currentSpace.path.forEach((position) => {
+        console.log(`  [${position}]`);
+      });
+      return currentSpace.path;
+    }
+
+    let nextMoves = getValidMoves(currentSpace.position);
+    nextMoves
+      .filter((coordinates) => !visited.has(coordinates.join()))
+      .forEach((coordinates) => {
+        visited.add(coordinates.join());
+        queue.push({
+          position: coordinates,
+          path: [...currentSpace.path, coordinates],
+        });
+      });
+  }
 }
+
+console.log(knightMoves([3, 3], [4, 3]));
 
 export { knightMoves };

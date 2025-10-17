13ed4d46afc9315c62e01517a10d0b093543aed3
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-16 22:07:52 -0500

    Refactor all funcs to destructure position arg

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-function getValidMoves(x, y) {
+function getValidMoves(position) {
+  const [x, y] = position;
   let moves = [
     [x + 2, y + 1],
     [x + 2, y - 1],
@@ -12,14 +13,15 @@ function getValidMoves(x, y) {
     [x - 1, y - 2],
   ];
 
-  return moves.filter((coordinates) =>
-    isValidPosition(coordinates[0], coordinates[1])
-  );
+  return moves.filter((coordinates) => isValidPosition(coordinates));
 }
 
-function isValidPosition(x, y) {
+function isValidPosition(position) {
+  const [x, y] = position;
   return x >= 0 && x <= 7 && y >= 0 && y <= 7;
 }
+
+console.log(getValidMoves([0, 0]));
 
 function knightMoves(start, end) {
   // main logic
